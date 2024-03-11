eaea6662141bf553f22803e8bd75406905b7b84f
hamdyadam97 <hamdyadam97@gmail.com>
2024-03-11 17:10:08 +0200

    model cart

---
+++ AliExpress.Models/Cart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliExpress.Models
+{
+    public class Cart
+    {
+
+        public int CartId { get; set; }
+        public int UserId { get; set; }
+        // Other cart properties
+
+        // Navigation properties
+        public User User { get; set; }
+        public ICollection<CartProduct> CartProducts { get; set; }
+    }
+}
