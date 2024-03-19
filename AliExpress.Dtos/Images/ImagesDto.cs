1b335089cd218916b0f4cf69ee5a07ae248d9eb1
hamdyadam97 <hamdyadam97@gmail.com>
2024-03-19 02:41:02 +0200

    immage

--- AliExpress.Dtos/Images/ImagesDto.cs
+++ AliExpress.Dtos/Images/ImagesDto.cs
@@ -9,8 +9,8 @@ namespace AliExpress.Dtos.Images
     public class ImagesDto
     {
         public string Url { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
+        //public string Title { get; set; }
+        //public string Description { get; set; }
+        //public decimal Price { get; set; }
     }
 }
--- AliExpress.Dtos/Product/ProductViewDto.cs
+++ AliExpress.Dtos/Product/ProductViewDto.cs
@@ -12,6 +12,6 @@ namespace AliExpress.Dtos.Product
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Image { get; set; }
+        public List<string> Image { get; set; }
     }
 }
