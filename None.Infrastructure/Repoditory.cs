437ecffe8592ea1bf51348878ef6cf034a418012
hamdyadam97 <hamdyadam97@gmail.com>
2024-03-19 15:14:21 +0200

    catx

--- None.Infrastructure/Repoditory.cs
+++ None.Infrastructure/Repoditory.cs
@@ -122,7 +122,13 @@ namespace None.Infrastructure
                         .Collection(p => p.Images)
                         .LoadAsync();
                 }
-
+                else if (entity is Category category)
+                {
+                    // Load related specifications if the entity is a Category
+                    await _context.Entry(category)
+                        .Collection(c => c.Specifications)
+                        .LoadAsync();
+                }
                 // Detach the entity to prevent accidental changes being tracked
                 _context.Entry(entity).State = EntityState.Detached;
             }
