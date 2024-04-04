777759b73736376c6c62e960488c9e794dd86dcd
hamdyadam97 <hamdyadam97@gmail.com>
2024-04-04 04:27:56 +0200

    order

--- AliExpress.Dtos/User/APIUserDTO.cs
+++ AliExpress.Dtos/User/APIUserDTO.cs
@@ -8,15 +8,15 @@ namespace AliExpress.Dtos.User
 {
     public class APIUserDTO
     {
-        public string Id { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; } // Make sure there is a setter for PhoneNumber
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string ?Email { get; set; }
+        public string? Phone { get; set; } // Make sure there is a setter for PhoneNumber
 
-        public string FName { get; set; }
-        public string LName { get; set; }
-        public string Gender { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string National { get; set; }
+        public string? FName { get; set; }
+        public string? LName { get; set; }
+        public string? Gender { get; set; }
+        public DateTime?  BirthDate { get; set; }
+        public string? National { get; set; }
     }
 }
